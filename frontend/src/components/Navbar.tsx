fe7de05465c784a6374a05073737dbce958bb101
Yash <sharmayash380@gmail.com>
2024-02-09 01:47:36 +0530

    changed the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
-
 const Navbar = () => {
   return (
-    <nav className=" w-2/3 flex justify-between px-5 py-2 font-poppins m-auto items-center text-[#1F2544] shadow-lg rounded-md border absolute top-5 left-2/4 -translate-x-2/4 bg-white">
+    <nav className=" w-5/6 flex justify-between px-5 py-2 font-poppins m-auto items-center text-[#1F2544] shadow-lg rounded-lg border bg-white mt-8">
       <div className=" flex items-center gap-2 cursor-pointer">
         <img
           src="./logo.png"
@@ -14,16 +12,9 @@ const Navbar = () => {
         </h3>
       </div>
       <div className="flex gap-8 items-center">
-        <Link to="/signup">
-          <button className=" px-[1em] py-[0.5em] border-2 border-slate-400  rounded-md">
-            <h1 className=" transition-all hover:scale-110"> Sign Up</h1>
-          </button>
-        </Link>
-        <Link to="/signin">
-          <button className="px-[1em] py-[0.5em] border-2 border-slate-400  rounded-md">
-            <h1 className=" transition-all hover:scale-110">Log In</h1>
-          </button>
-        </Link>
+        <button className=" px-[1em] py-[0.5em] border-2 border-slate-400  rounded-md">
+          <h1 className=" transition-all hover:scale-110"> Logout</h1>
+        </button>
       </div>
     </nav>
   );
